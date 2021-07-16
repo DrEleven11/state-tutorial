705efc60d01a37be0db7dbc7fb2559f722239eb8
Andrew <andrew.kent.90@gmail.com>
2021-07-16 07:30:31 -0700

    new tutorial code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import ButtonCSS from './initline';
+import { useState } from 'react';
 
 function App() {
+
+  const [data,setData] = useState([{id:'1234',cmark:false,dmark:false,rmark:false},{id:'5678',cmark:false,dmark:false,rmark:false},{id:'91011',cmark:false,dmark:false,rmark:false}])
+
+  function update_state(id,value,item){
+  
+   console.log(id,value,item)
+    let array_index =  data.findIndex((line) => {
+      return line.id === id 
+    })
+    let newstate = [...data]
+    newstate[array_index][item] = value
+    setData(newstate)
+
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {data.map(record => <><ButtonCSS buttoncss={record} update_state={update_state}></ButtonCSS><br/></>)}
     </div>
   );
 }
